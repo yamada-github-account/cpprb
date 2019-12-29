7fd9fae8c8d47eeb65504727f9da4e63da8f7493
Yamada <>
2019-12-29 11:55:09 +0900

    Add: utility header for design study

---
+++ design_study/utility.hh
@@ -0,0 +1,38 @@
+#ifndef CPPRB_UTILITY_HH
+#define CPPRB_UTILITY_HH
+
+#include <chrono>
+#include <cmath>
+#include <utility>
+
+class Stat {
+  unsigned long long N;
+  long double x;
+  long double xx;
+
+public:
+  Stat(): N{0},x{0},xx{0} {}
+  void Add(double _x){
+    ++N;
+    x += _x;
+    xx += _x*_x;
+  }
+  double Average() const {
+    return (double) x/N;
+  }
+  double Std() const {
+    auto ave = Average();
+    return (double)std::sqrt(xx/N - ave*ave);
+  }
+};
+
+template<typename F,typename...Args> auto measure(F&& f,Args&&...args){
+  auto start = std::chrono::high_resolution_clock::now();
+  f(std::forward<Args>(args)...);
+  auto end = std::chrono::high_resolution_clock::now();
+
+  return std::duration_cast<std::chrono::nanoseconds>(end - start).count();
+}
+
+
+#endif // CPPRB_UTILITY
